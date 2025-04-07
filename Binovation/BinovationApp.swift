9a9ec0f94a8d71008e0bc244ec73e6b9113b4bf7
KuKaH <kukah99@gmail.com>
2025-04-08 03:12:52 +0900

    Initial commit

---
+++ Binovation/BinovationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinovationApp.swift
+//  Binovation
+//
+//  Created by 홍준범 on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BinovationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Binovation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Binovation
+//
+//  Created by 홍준범 on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Binovation")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
