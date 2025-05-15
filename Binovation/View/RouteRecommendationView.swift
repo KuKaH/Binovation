c24c05795b2cde808f5f32a09e541b5637bab02f
KuKaH <kukah99@gmail.com>
2025-05-15 15:47:10 +0900

    동선추천 화면 틀 만들기

--- Binovation/View/RouteRecommendationView.swift
+++ Binovation/View/RouteRecommendationView.swift
@@ -9,7 +9,64 @@ import SwiftUI
 
 struct RouteRecommendationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 24) {
+                Text("동선 추천")
+                    .font(.title)
+                    .bold()
+                    .padding(.top)
+                
+                
+                
+                VStack(alignment: .leading, spacing: 16) {
+                    HStack {
+                        Image(systemName: "figure.walk")
+                            .foregroundStyle(.blue)
+                        Text("최적 동선을 추천드립니다!")
+                            .font(.headline)
+                            .foregroundStyle(Color.blue)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack {
+                            Text("추천 동선")
+                                .bold()
+                            Spacer()
+                            Text("사회과학관 -> 도서관 -> 사이버관")
+                        }
+                        
+                        Text("예상 소요시간: 30분")
+                        Text("처리해야 할 쓰레기통 개수: 4개")
+                        
+                        Divider()
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("세부사항").bold()
+                            Text("사회과학관 5층 -> 4층 -> 3층")
+                            Text("사회과학관 5층 -> 1층")
+                            Text("사이버관 3층 -> 2층")
+                        }
+                    }
+                    .font(.subheadline)
+                    .padding()
+                    .background(Color.blue.opacity(0.1))
+                    .cornerRadius(16)
+                }
+                
+                VStack {
+                    Text("현재 위치를 지도에서 확인하세요!")
+                        .foregroundStyle(.gray)
+                        .font(.title2)
+                    Image("HUFSMiniMap")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(12)
+                        .shadow(radius: 4)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 80)
+        }
     }
 }
 
